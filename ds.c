8229657a5c82426e9f4792e73d382b665f66ac5e
dZONE <abhiroop11@yahoo.com>
2014-09-06 22:11:15 -0400

    Trie inser & search

--- ds.c
+++ ds.c
@@ -42,3 +42,78 @@ Node *seek_stack(Stack **top)
     pt = (Node *) tmp->ptr;
     return pt;
 }
+
+Trie *trie_newNode(void)
+{
+    Trie *tmp = NULL;
+    int i = 0;
+
+    tmp = malloc(sizeof(Trie));
+    if ( !tmp )
+	return NULL;
+    else {
+	for ( i = 0; i<26; i++ )
+	    tmp->elm[i] = NULL;
+	tmp->end = 0;
+    }
+
+    return tmp;
+}
+
+/* Lower case english alphabets */
+void trie_insert(Trie *root, char *arr)
+{
+    char *tmp = arr;
+    Trie *cur = root;
+    int index;
+
+    int len = 0, i = 0;
+
+    if ( !arr )
+	return;
+
+    while( *tmp != '\0' ) {
+	++len;
+	++tmp;
+    }
+    tmp = arr;
+
+    for ( i = 0; i<len; i++ )
+    {
+	index = tmp[i] - 'a';
+
+	if ( !cur->elm[index] ) 
+	    cur->elm[index] = trie_newNode();
+
+	cur = cur->elm[index];
+    }
+    cur->end = 1;
+}
+
+/* Return 0 if not found, 1 if found, -1 if error */
+int trie_search(Trie *root, char *arr)
+{
+    Trie *cur = root;
+    char *tmp = arr;
+    int i = 0, len = 0, index;
+
+    if ( !cur || !arr )
+	return -1;
+
+    while( *tmp != '\0' ) {
+	++len;
+	++tmp;
+    }
+
+    tmp = arr;
+    for ( i = 0; i<len; i++ ) 
+    {
+	index = tmp[i] - 'a';
+	if ( !cur->elm[index] )
+	    return 0;
+	cur = cur->elm[index];
+    }
+
+    if ( cur->end )
+	return 1;
+}
--- ds.h
+++ ds.h
@@ -13,12 +13,20 @@ typedef struct node {
 } Node;
 
 
+typedef struct trie {
+    struct trie *elm[26];
+    int end;
+} Trie;
+
 typedef l_node Stack;
 typedef l_node Que;
 
 Stack *push(Stack **top, Node *node);
 Node *pop(Stack **top);
 Node *seek_stack(Stack **top);
+
+void trie_insert(Trie *root, char *arr);
+int trie_search(Trie *root, char *arr);
 
 
 
